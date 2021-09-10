3aaf056eea92e1c2754be83785108f206c2ac444
Alex <alejandro.onofre@bloque9.com>
2021-09-10 13:43:51 -0500

    integrated version 10 sep 2021

---
+++ resources/js/change-img.js
@@ -0,0 +1,4 @@
+$(".list li a").hover( function() { // Changes the .image-holder's img src to the src defined in .list a's data attribute.
+    var value=$(this).attr('data-src');
+    $(".image-holder img").attr("src", value);
+});
