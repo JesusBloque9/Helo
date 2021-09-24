d94a6ca1252173e8478eee5d7a62fe3083010ef3
root <daniel.m.arvizu@gmail.com>
2021-09-24 10:54:53 -0500

    menu sticky on home

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     setTimeout(function() {
       $('#ctn-preloader').addClass('loaded');
-      // Una vez haya terminado el preloader aparezca el scroll
-      $('body').removeClass('no-perspective');  
       if ($('#ctn-preloader').hasClass('loaded')) {
         // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
         $('#preloader').delay(1000).queue(function() {
@@ -11,12 +9,11 @@ $(document).ready(function() {
       }
     }, 3000);
 });
-$("#openNav").click(function() {
+$(".openNav").click(function() {
   $(".page").addClass("swipe-lft");
   $(".nav").addClass("open");
 });
-$("#closeNav").click(function() {
-  alert("hola");
+$(".closeNav").click(function() {
   $(".page").removeClass("swipe-lft");
   $(".nav").removeClass("open");
 });
