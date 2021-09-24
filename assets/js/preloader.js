05b6184271ff6c3732e017beb53c2acf25109afa
root <daniel.m.arvizu@gmail.com>
2021-09-24 09:19:56 -0500

    preloader fixed

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  
     setTimeout(function() {
       $('#ctn-preloader').addClass('loaded');
       // Una vez haya terminado el preloader aparezca el scroll
-      $('body').removeClass('no-scroll-y');  
+      $('body').removeClass('no-perspective');  
       if ($('#ctn-preloader').hasClass('loaded')) {
         // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
         $('#preloader').delay(1000).queue(function() {
@@ -11,16 +10,13 @@ $(document).ready(function() {
         });
       }
     }, 3000);
-    
-  });
-
-
-  $("#openNav").click(function() {
-    $(".page").addClass("swipe-lft");
-    $(".nav").addClass("open");
-  });
-  $("#closeNav").click(function() {
-    alert("hola");
-    $(".page").removeClass("swipe-lft");
-    $(".nav").removeClass("open");
-  });
+});
+$("#openNav").click(function() {
+  $(".page").addClass("swipe-lft");
+  $(".nav").addClass("open");
+});
+$("#closeNav").click(function() {
+  alert("hola");
+  $(".page").removeClass("swipe-lft");
+  $(".nav").removeClass("open");
+});
