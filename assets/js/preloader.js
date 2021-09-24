2064c6230fc01afe7de06f2b0ea67206031da29b
root <daniel.m.arvizu@gmail.com>
2021-09-24 17:00:55 -0500

    preloaded hiden when resource was loaded

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-    setTimeout(function() {
-      $('#ctn-preloader').addClass('loaded');
-      if ($('#ctn-preloader').hasClass('loaded')) {
-        // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
-        $('#preloader').delay(1000).queue(function() {
-          $(this).remove();
-        });
-      }
-    }, 3000);
+  $('#ctn-preloader').addClass('loaded');
+  if ($('#ctn-preloader').hasClass('loaded')) {
+    // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
+    $('#preloader').delay(1000).queue(function() {
+      $(this).addClass('d-none');
+    });
+  }
 });
 $(".openNav").click(function() {
   $(".page").addClass("swipe-lft");
