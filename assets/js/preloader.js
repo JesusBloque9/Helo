fbe1b29ed26ce8127a6d38c8985b7cc0c4313bb9
root <daniel.m.arvizu@gmail.com>
2021-09-28 16:40:50 -0500

    option for hide preload after loaded all resources

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
     });
   }
 });
+
+/*$(window).on('load', function () {
+   $('#ctn-preloader').addClass('loaded');
+   if ($('#ctn-preloader').hasClass('loaded')) {
+     // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
+     $('#preloader').delay(1000).queue(function() {
+       $(this).addClass('d-none');
+     });
+   }
+});*/
+
+
 $(".openNav").click(function() {
   $(".page").addClass("swipe-lft");
   $(".nav").addClass("open");
